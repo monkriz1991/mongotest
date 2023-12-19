619352afb0e4c1ac259a906333ed31707c594518
krizis <monkriz1991@gmail.com>
2023-12-19 19:41:38 +0300

    tg

--- server/models/add/AddPost.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-// const postScema = new mongoose.Schema({
-//     title: { type: String, require: true,trim:true },
-//   });
-
-// const PostModel = mongoose.model('posts',postScema)
-
-// export default PostModel
